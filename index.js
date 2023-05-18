50dd4fc7e56d70edfef8ede6175024e841dc6177
nsamedaisy <nsamedaisy@gmail.com>
2023-05-18 15:21:05 +0100

    react-playground commit

--- index.js
+++ index.js
@@ -38,8 +38,6 @@
 
 
 
-
-
 // function Element(props) {
 //   const personality = props.personality
 //   return (
@@ -124,25 +122,3 @@
 
 
 
-
-
-rootElement = document.getElementById('root')
-function Todo() {
-  const [todo, setTodo] = React.useState(0)
-  const [] = React.useState(0)
-
-  const addNewTodo = () => {}
-  const deleteTodo = () => {}
-  const numbersOfTodo = () => {}
-
-  return (
-    <>
-    <h1>Todo List App</h1>
-    <div>
-      <input type="text" placeholder="create new todo"/>
-      <img src='../addbtn.png' alt='logo' />
-    </div>
-    </>
-  )
-}
-ReactDOM.render(<Todo />, rootElement)
--- todo.js
+++ todo.js
@@ -1,20 +1,49 @@
-// rootElement = document.getElementById('root')
-// function Todo() {
-//   const [] = React.useState(0)
-//   const [] = React.useState(0)
+// Build a simple todo app where the user can add new todos, update todos, delete todos, display the total number of todos.
+const rootElement = document.getElementById('root')
+function Todo() {
+  const [todo, setTodo] = React.useState("")
+  const [todos, setTodos] = React.useState([])
 
-//   const addNewTodo = () => {}
-//   const deleteTodo = () => {}
-//   const numbersOfTodo = () => {}
+  const addNewTodo = () => {
+    if (todo !== '') {
+      setTodos([... todos, todo])
+      console.log(todo)
+      setTodo('')
+    }
+  }
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text
+    })
+    setTodos(newTodos)
+  }
 
-//   return (
-//     <>
-//     <h1>Todo List App</h1>
-//     <div>
-//       <input type="text" placeholder="create new todo"/>
-//       <img src='../addbtn.png' alt='logo' />
-//     </div>
-//     </>
-//   )
-// }
-// ReactDOM.render(<Todo />, rootElement)
+  const updateTodo = () => {}
+
+  return (
+    <div>
+    <h1>Todo List App</h1>
+    <div>
+      <input type="text" placeholder="create new todo" value={todo} onChange={(event) => {setTodo(event.target.value)}}/>
+      <button onClick={addNewTodo}>add</button>
+    </div>
+{/* display field */}
+    <ul>
+      {todos.map((todo)=>(
+        <li key={todo}>{todo}{<button onClick={() => deleteTodo(todo)}>delete</button>}{<button onClick={updateTodo}>edit</button>}
+        </li>
+      ))}
+
+    </ul>
+    </div>
+  )
+}
+ReactDOM.render(<Todo />, rootElement)
+
+
+//onchange event handler triggers everytime my todo changes 
+//onChange provides an events which is then use to get the value using event.target.value
+
+//I used a spread operator to add todo items to the todos array because the todo state should never be mutated
+
+//Array.map() method takes data transform to list view
